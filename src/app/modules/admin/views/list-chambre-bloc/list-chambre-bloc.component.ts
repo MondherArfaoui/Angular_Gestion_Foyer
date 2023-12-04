f9fc0cf2bb60d115a8e116f8e85267a8e2b8dc1c
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-12-04 16:02:07 +0100

    feat: implement CRUD functionality in list-chambre-bloc component

--- src/app/modules/admin/views/list-chambre-bloc/list-chambre-bloc.component.ts
+++ src/app/modules/admin/views/list-chambre-bloc/list-chambre-bloc.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Bloc } from 'src/app/core/models/bloc/bloc';
 import { Chambre } from 'src/app/core/models/chambre/chambre';
+import { TypeChambre } from 'src/app/core/models/TypeChambre/type-chambre.enum';
+import { BlocService } from 'src/app/core/services/bloc/bloc.service';
+
 import { ChambreService } from 'src/app/core/services/chambre/chambre.service';
 
+interface SelectedRoom {
+  idChambre?: number;
+  numeroChambre?: string;
+  typeC?: TypeChambre;
+  bloc?: Bloc;
+}
 @Component({
   selector: 'app-list-chambre-bloc',
   templateUrl: './list-chambre-bloc.component.html',
@@ -14,13 +24,27 @@ export class ListChambreBlocComponent implements OnInit {
   showUpdateRoomForm = false;
   showRoomForm = false;
 
-  selectedRoom: Partial<Chambre> | null = null; // Change here
+  selectedRoom: SelectedRoom | null = null;
+
+  typeChambreEnum: string[] = Object.values(TypeChambre);
+
+  blocks: Bloc[] = [];
+
+  newRoom: any = {
+    numeroChambre: 0,
+    typeC: this.typeChambreEnum[0],
+    bloc: new Bloc()
+  };
 
 
-  constructor(private chambreService: ChambreService) { }
+
+
+  constructor(private chambreService: ChambreService,
+    private blocService: BlocService,) { }
 
   ngOnInit(): void {
     this.loadChambres();
+    this.loadBlocs();
   }
 
   loadChambres() {
@@ -34,14 +58,37 @@ export class ListChambreBlocComponent implements OnInit {
     );
   }
 
+  loadBlocs() {
+    this.blocService.getAllBlocs().subscribe(
+      (data: Bloc[]) => {
+        this.blocks = data;
+      },
+      (error) => {
+        console.error('Error fetching bloc', error);
+      }
+    )
+  }
+
   displayChambre(chambreId: number) {
-    // Implement logic for displaying the room with ID: chambreId
-    console.log(`Displaying chambre with ID: ${chambreId}`);
   }
 
   deleteChambre(chambreId: number) {
-    // Implement logic for deleting the room with ID: chambreId
-    console.log(`Deleting chambre with ID: ${chambreId}`);
+    const confirmDelete = confirm('Are you sure you want to delete this room?');
+
+    if (!confirmDelete) {
+      return;
+    }
+
+    this.chambreService.deleteChambre(chambreId).subscribe(
+      (response) => {
+        console.log(`Server response:`, response);
+        console.log(`Room with ID ${chambreId} deleted successfully`);
+        this.loadChambres();
+      },
+      (error) => {
+        console.error(`Error deleting room with ID ${chambreId}`, error);
+      }
+    );
   }
 
   updateChambre(chambreId: number) {
@@ -54,15 +101,24 @@ export class ListChambreBlocComponent implements OnInit {
     this.showUpdateRoomForm = false;
     this.showRoomForm = false;
 
-  }
+    this.chambreService.addChambre(this.newRoom).subscribe(
+      (response) => {
+        console.log('Room added successfully', response);
+        this.loadChambres();
+        this.cancelAddChambre();
+        this.newRoom = {
+          numeroChambre: 0,
+          typeC: this.typeChambreEnum[0],
+          bloc: new Bloc()
+        };
 
-  cancelAddChambre() {
-    this.showAddRoomForm = false;
-    this.showUpdateRoomForm = false;
-    this.showRoomForm = false;
-    this.selectedRoom = null;
-  }
+      },
+      (error) => {
+        console.error('Error adding room', error);
+      }
+    );
 
+  }
 
   viewChambreDetails(chambre: any) {
     this.selectedRoom = chambre;
@@ -76,10 +132,38 @@ export class ListChambreBlocComponent implements OnInit {
     this.showUpdateRoomForm = true;
     this.showRoomForm = false;
     this.showAddRoomForm = false;
+  }
+  updateRoom() {
+
+    if (!this.selectedRoom) {
+      return;
+    }
+
+    const updatedRoom = {
+      idChambre: this.selectedRoom.idChambre ?? 0,  // Provide a default value, adjust as needed
+      numeroChambre: this.selectedRoom.numeroChambre ?? '',
+      typeC: this.selectedRoom.typeC ?? TypeChambre,  // Provide a default value, adjust as needed
+      bloc: {
+        idBloc: this.selectedRoom.bloc?.idBloc ?? 0,  // Use optional chaining to handle potential null or undefined
+        nomBloc: this.selectedRoom.bloc?.nomBloc ?? '',
+        // add other properties if needed
+      },
+    };
+
+    this.chambreService.updateChambre(updatedRoom).subscribe(
+      (response) => {
+        console.log(`Room with ID ${this.selectedRoom!.idChambre} updated successfully`, response);
+        this.loadChambres();
+        this.cancelAddChambre();
+      },
+      (error) => {
+        console.error(`Error updating room with ID ${this.selectedRoom!.idChambre}`, error);
+      }
+    );
+
 
   }
 
-  // Method to safely update selectedRoom properties
   updateSelectedRoom(property: keyof Chambre, value: any) {
     // this.selectedRoom = value;
     // this.showUpdateRoomForm = true;
@@ -89,4 +173,12 @@ export class ListChambreBlocComponent implements OnInit {
       this.selectedRoom[property] = value;
     }
   }
+
+  cancelAddChambre() {
+    this.showAddRoomForm = false;
+    this.showUpdateRoomForm = false;
+    this.showRoomForm = false;
+    this.selectedRoom = null;
+  }
+
 }
